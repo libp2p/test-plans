3799d870cc2e2c18b755f2d7c7ad5796452b4af1
Max Inden <mail@max-inden.de>
2022-04-07 18:10:00 +0200

    compatibility/rust: Set IP address instead of relying on assigned

--- compatibility/compatibility-rust/src/main.rs
+++ compatibility/compatibility-rust/src/main.rs
@@ -7,51 +7,74 @@ const LISTENING_PORT: u16 = 1234;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
-    let mut sync_client = testground::sync::Client::new().await?;
+    let (client, run_params) = testground::client::Client::new().await?;
 
-    let local_addr = &if_addrs::get_if_addrs()
-        .unwrap()
-        .into_iter()
-        .find(|iface| iface.name == "eth1")
-        .unwrap()
-        .addr
-        .ip();
+    client.wait_network_initialized().await?;
 
-    match local_addr {
-        std::net::IpAddr::V4(addr) if addr.octets()[3] % 2 == 0 => {
+    let seq = client
+        .signal_and_wait("ip-allocation", run_params.test_instance_count)
+        .await?;
+
+    println!("Seq is: {:?}", seq);
+
+    client
+        .configure_network(testground::network_conf::NetworkConfiguration {
+            network: "default".to_string(),
+            ipv4: Some(
+                ipnetwork::Ipv4Network::new(
+                    Ipv4Addr::new(16, 1, ((seq >> 8) + 1) as u8, seq as u8),
+                    24,
+                )
+                .unwrap(),
+            ),
+            ipv6: None,
+            enable: true,
+            default: testground::network_conf::LinkShape {
+                latency: 10000000,
+                jitter: 0,
+                bandwidth: 1048576,
+                filter: testground::network_conf::FilterAction::Accept,
+                loss: 0.0,
+                corrupt: 0.0,
+                corrupt_corr: 0.0,
+                reorder: 0.0,
+                reorder_corr: 0.0,
+                duplicate: 0.0,
+                duplicate_corr: 0.0,
+            },
+            rules: None,
+            callback_state: "network-configured".to_string(),
+            callback_target: None,
+            routing_policy: testground::network_conf::RoutingPolicyType::AllowAll,
+        })
+        .await?;
+
+    match seq {
+        1 => {
             println!("Test instance, listening for incoming connections.");
 
-            let listener = TcpListener::bind((*addr, LISTENING_PORT))?;
+            let listener = TcpListener::bind(("0.0.0.0", LISTENING_PORT))?;
 
-            sync_client.signal("listening".to_string()).await?;
+            client.signal("listening".to_string()).await?;
 
-            listener
-                .incoming()
-                .next()
-                .expect("Listener not to close.")?;
-            println!("Established inbound TCP connection.");
+            let mut connections = listener.incoming();
+
+            for _ in 0..(run_params.test_instance_count - 1) {
+                connections.next().expect("Listener not to close.")?;
+                println!("Established inbound TCP connection.");
+            }
         }
-        std::net::IpAddr::V4(addr) if addr.octets()[3] % 2 != 0 => {
+        _ => {
             println!("Test instance, connecting to listening instance.");
 
-            sync_client
-                .wait_for_barrier("listening".to_string(), 1)
-                .await?;
+            client.barrier("listening".to_string(), 1).await?;
 
-            let remote_addr: Ipv4Addr = {
-                let mut octets = addr.octets();
-                octets[3] = octets[3] - 1;
-                octets.into()
-            };
-            let _stream = TcpStream::connect((remote_addr, LISTENING_PORT)).unwrap();
+            let _stream = TcpStream::connect((Ipv4Addr::new(16, 1, 1, 1), LISTENING_PORT)).unwrap();
             println!("Established outbound TCP connection.");
-        }
-        addr => {
-            panic!("Unexpected local IP address {:?}", addr);
         }
     }
 
-    sync_client.publish_success().await?;
+    client.record_success().await?;
 
     Ok(())
 }
