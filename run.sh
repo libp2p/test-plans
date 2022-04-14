5b444796327977291f5036fe6bd8ab6dd9656b37
Laurent Senta <laurent@singulargarden.com>
2022-04-14 13:04:17 +0200

    ci: add run feedback

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ set -e
 set -o pipefail
 set -x
 
+# See original code:
+# https://github.com/galargh/testground-github-action/blob/master/entrypoint.sh
+
 testground healthcheck --runner local:docker --fix
 testground run composition -f ./compatibility/composition.toml  \
     --metadata-repo "${GITHUB_REPOSITORY}"            \
@@ -21,3 +24,35 @@ done
 
 echo "terminating remaining containers"
 testground terminate --runner local:docker
+
+echo -n "Testground ended: "; date
+
+echo getting extended status
+testground status -t "${TGID}" --extended  | tee extendedstatus.out
+
+# Get the extened status, which includes a "Result" section.
+# Capture the line that occurs after "Result:"
+extstatus=$(awk '/Result/ {getline; print $0}' <extendedstatus.out)
+
+# First off, there are control characters in this output, and we need to remove that.
+# https://github.com/testground/testground/issues/1214
+extstatus=$(echo "${extstatus}" | tr -d "[:cntrl:]" |  sed 's/\[0m//g')
+
+# test if we got a result at all. The result might be "null". A null result means most likely the
+# job was canceled before it began for some reason.
+if [ "${extstatus}" == "null" ]
+then
+	echo "${OUTPUT_OUTCOME}failure/canceled"
+	exit 1
+fi
+
+# Now find the outcome of the test. The extended result is going to look something like this:
+# {"journal":{"events":{},"pods_statuses":{}},"outcome":"success","outcomes":{"providers":{"ok":1,"total":1},"requestors":{"ok":1,"total":1}}}
+
+outcome=$(echo "${extstatus}" | jq ".outcome")
+
+echo "the extended status was ${extstatus}"
+echo "The outcome of this test was ${outcome}"
+echo "${OUTPUT_OUTCOME}${outcome}"
+
+test "${outcome}" = "\"success\"" && exit 0 || exit 1
