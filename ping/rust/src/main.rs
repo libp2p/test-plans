2f3001595859e187d75c7a8353cf1c2191977d48
Max Inden <mail@max-inden.de>
2022-07-18 22:04:33 +0900

    ping/rust: Implement iterations with latency

--- ping/rust/src/main.rs
+++ ping/rust/src/main.rs
@@ -4,6 +4,10 @@ use std::time::Duration;
 
 use env_logger::Env;
 use log::info;
+use rand::Rng;
+use testground::network_conf::{
+    FilterAction, LinkShape, NetworkConfiguration, RoutingPolicyType, DEFAULT_DATA_NETWORK,
+};
 
 pub mod libp2p {
     #[cfg(all(feature = "libp2pv0470",))]
@@ -28,10 +32,11 @@ const LISTENING_PORT: u16 = 1234;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info,libp2p_core=info")).init();
 
     let client = testground::client::Client::new_and_init().await?;
 
+    // TODO: Still needed?
     let num_other_instances = client.run_parameters().test_instance_count as usize - 1;
 
     let mut swarm = {
@@ -130,9 +135,73 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
+    ping(&client, &mut swarm, "initial".to_string()).await?;
+
+    let iterations: usize = client
+        .run_parameters()
+        .test_instance_params
+        .get("iterations")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let max_latency_ms: u64 = client
+        .run_parameters()
+        .test_instance_params
+        .get("max_latency_ms")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    for i in 0..iterations {
+        client.record_message(format!("⚡️  ITERATION ROUND {}", i));
+
+        let latency = Duration::from_millis(rand::thread_rng().gen_range(0..max_latency_ms))
+            .as_nanos()
+            .try_into()
+            .unwrap();
+
+        let network_conf = NetworkConfiguration {
+            network: DEFAULT_DATA_NETWORK.to_owned(),
+            ipv4: None,
+            ipv6: None,
+            enable: true,
+            default: LinkShape {
+                latency,
+                jitter: 0,
+                bandwidth: 0,
+                filter: FilterAction::Accept,
+                loss: 0.0,
+                corrupt: 0.0,
+                corrupt_corr: 0.0,
+                reorder: 0.0,
+                reorder_corr: 0.0,
+                duplicate: 0.0,
+                duplicate_corr: 0.0,
+            },
+            rules: None,
+            callback_state: format!("network-configured-{}", i),
+            callback_target: Some(client.run_parameters().test_instance_count),
+            routing_policy: RoutingPolicyType::AllowAll,
+        };
+
+        client.configure_network(network_conf).await.unwrap();
+
+        ping(&client, &mut swarm, format!("done-{}", i)).await?;
+    }
+
+    client.record_success().await?;
+
+    Ok(())
+}
+
+async fn ping(
+    client: &testground::client::Client,
+    swarm: &mut Swarm<ping::Behaviour>,
+    tag: String,
+) -> Result<(), Box<dyn std::error::Error>> {
     info!("Wait to receive ping from each peer.");
     let mut pinged = HashSet::new();
-    while pinged.len() < num_other_instances {
+    while pinged.len() < client.run_parameters().test_instance_count as usize - 1 {
         let event = swarm.next().await.unwrap();
         info!("Event: {:?}", event);
         if let SwarmEvent::Behaviour(ping::PingEvent {
@@ -144,18 +213,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    info!("Wait for all peers to signal being done with \"initial\" round.");
+    info!(
+        "Wait for all peers to signal being done with \"{}\" round.",
+        tag
+    );
     swarm
         .take_until(
             client
-                .signal_and_wait("initial", client.run_parameters().test_instance_count)
+                .signal_and_wait(tag, client.run_parameters().test_instance_count)
                 .boxed_local(),
         )
         .map(|event| info!("Event: {:?}", event))
         .collect::<Vec<()>>()
         .await;
-
-    client.record_success().await?;
 
     Ok(())
 }
