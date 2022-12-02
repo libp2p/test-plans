67baab5254f4941a8316919461a1868ec731e1fb
Laurent Senta <laurent@singulargarden.com>
2022-12-02 15:36:52 +0100

    generate-dashboard: move matcher to lib file

--- .github/actions/generate-dashboard/src/lib.spec.ts
+++ .github/actions/generate-dashboard/src/lib.spec.ts
@@ -1,16 +1,4 @@
-type PairOfImplementation = [string, string]
-
-// `     something x something-lese    more info ignore`
-const TASK_ID_MATCHER = /\s*(\S+)\s+x\s+(\S+)\s*.*/;
-
-const fromTaskIdToCoordinate = (taskId: string): PairOfImplementation => {
-  const match = taskId.match(TASK_ID_MATCHER);
-  if (!match) {
-    throw new Error(`Task ID ${taskId} does not match the expected format`);
-  }
-
-  return [match[1], match[2]];
-};
+import { fromTaskIdToCoordinate } from "./lib";
 
 describe("conversion", () => {
   it.each(["random id", "almost xtherexfriend"])(
@@ -23,12 +11,15 @@ describe("conversion", () => {
   it.each([
     ["someid x anotherid", ["someid", "anotherid"]],
     ["go-v0.21 x go-v0.20", ["go-v0.21", "go-v0.20"]],
-    ["someid2 x anotherid2 (with additional parameters)", ["someid2", "anotherid2"]],
-    ["someid x anotherid (with additional parameters)", ["someid", "anotherid"]],
-  ])(
-    "should parse pairs correctly: %s => %s",
-    (taskId, pairs) => {
-      expect(fromTaskIdToCoordinate(taskId)).toEqual(pairs);
-    }
-  );
+    [
+      "someid2 x anotherid2 (with additional parameters)",
+      ["someid2", "anotherid2"],
+    ],
+    [
+      "someid x anotherid (with additional parameters)",
+      ["someid", "anotherid"],
+    ],
+  ])("should parse pairs correctly: %s => %s", (taskId, pairs) => {
+    expect(fromTaskIdToCoordinate(taskId)).toEqual(pairs);
+  });
 });
--- .github/actions/generate-dashboard/src/lib.ts
+++ .github/actions/generate-dashboard/src/lib.ts
@@ -7,6 +7,7 @@ type ResultLine = {
   outcome: string;
   error: string;
 };
+
 export type ResultFile = ResultLine[];
 
 export const load = (path: string): ResultFile => {
@@ -19,4 +20,20 @@ export const load = (path: string): ResultFile => {
 
 export const save = (path: string, content: string) => {
   fs.writeFileSync(path, content);
+};
+
+type PairOfImplementation = [string, string];
+
+// `     something x something-else    more info ignore`
+const TASK_ID_MATCHER = /\s*(\S+)\s+x\s+(\S+)\s*.*/;
+
+export const fromTaskIdToCoordinate = (
+  taskId: string
+): PairOfImplementation => {
+  const match = taskId.match(TASK_ID_MATCHER);
+  if (!match) {
+    throw new Error(`Task ID ${taskId} does not match the expected format`);
+  }
+
+  return [match[1], match[2]];
 };
