c47e8149dd77b76805804a9c319afed2c3ba992c
Laurent Senta <laurent@singulargarden.com>
2022-12-02 15:36:08 +0100

    generated-dashboard: add task id matcher

--- .github/actions/generate-dashboard/src/lib.spec.ts
+++ .github/actions/generate-dashboard/src/lib.spec.ts
@@ -1,17 +1,34 @@
-interface ICoordinates {
-  x: number;
-  y: number;
-}
+type PairOfImplementation = [string, string]
 
-const fromTaskIdToCoordinate = (taskId: string): ICoordinates => {
-  throw new Error("Not implemented");
+// `     something x something-lese    more info ignore`
+const TASK_ID_MATCHER = /\s*(\S+)\s+x\s+(\S+)\s*.*/;
+
+const fromTaskIdToCoordinate = (taskId: string): PairOfImplementation => {
+  const match = taskId.match(TASK_ID_MATCHER);
+  if (!match) {
+    throw new Error(`Task ID ${taskId} does not match the expected format`);
+  }
+
+  return [match[1], match[2]];
 };
 
-describe("convesion", () => {
-  it.each(["random id", "almost x there x friend"])(
+describe("conversion", () => {
+  it.each(["random id", "almost xtherexfriend"])(
     "should throw on invalid taskId: %s",
     (taskId) => {
       expect(() => fromTaskIdToCoordinate(taskId)).toThrow();
+    }
+  );
+
+  it.each([
+    ["someid x anotherid", ["someid", "anotherid"]],
+    ["go-v0.21 x go-v0.20", ["go-v0.21", "go-v0.20"]],
+    ["someid2 x anotherid2 (with additional parameters)", ["someid2", "anotherid2"]],
+    ["someid x anotherid (with additional parameters)", ["someid", "anotherid"]],
+  ])(
+    "should parse pairs correctly: %s => %s",
+    (taskId, pairs) => {
+      expect(fromTaskIdToCoordinate(taskId)).toEqual(pairs);
     }
   );
 });
