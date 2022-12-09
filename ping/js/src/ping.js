85f8e7f679aba372d6598b3cb0b9d2b71b09b037
glendc <glen.decauwsemaecker@otainsight.com>
2022-12-09 21:26:46 +0100

    update ping js code

--- ping/js/src/ping.js
+++ ping/js/src/ping.js
@@ -1,4 +1,5 @@
 const { network } = require('@testground/sdk')
+const { PingService } = require('libp2p')
 
 function sleep (ms) {
   return new Promise((resolve) => {
@@ -18,57 +19,37 @@ function getRandom (min, max) {
 // The leader waits until all the followers have reached the state "ready"
 // then, the followers wait for a signal from the leader to be released.
 module.exports = async (runenv, client) => {
-  const enrolledState = 'enrolled'
-  const readyState = 'ready'
-  const releasedState = 'released'
+	// 📝  Consume test parameters from the runtime environment.
+  const secureChannel = runenv.runParams.testInstanceParams['secure_channel']
+  const maxLatencyMs = runenv.runParams.testInstanceParams['max_latency_ms']
+  const iterations = runenv.runParams.testInstanceParams['iterations']
+
+	runenv.recordMessage(`started test instance; params: secure_channel=${secureChannel}, max_latency_ms=${maxLatencyMs}, iterations=${iterations}`)
 
   // instantiate a network client; see 'Traffic shaping' in the docs.
-  const netclient = network.newClient(client, runenv)
+  const netClient = network.newClient(client, runenv)
   runenv.recordMessage('waiting for network initialization')
 
   // wait for the network to initialize; this should be pretty fast.
-  netclient.waitNetworkInitialized()
+  await netClient.waitNetworkInitialized()
   runenv.recordMessage('network initilization complete')
 
-  // signal entry in the 'enrolled' state, and obtain a sequence number.
-  const seq = await client.signalEntry(enrolledState)
-  runenv.recordMessage(`my sequence ID: ${seq}`)
+  // We need to listen on (and advertise) our data network IP address, so we
+	// obtain it from the NetClient.
+	const ip = netClient.getDataNetworkIP()
 
-  // if we're the first instance to signal, we'll become the LEADER.
-  if (seq === 1) {
-    runenv.recordMessage('i am the leader.')
-    const numFollowers = runenv.testInstanceCount - 1
+	// ☎️  Let's construct the libp2p node.
+	const listenAddr = `/ip4/${ip}/tcp/0`
+	// host, err := compat.NewLibp2(ctx,
+	// 	secureChannel,
+	// 	libp2p.ListenAddrStrings(listenAddr),
+	// )
 
-    // let's wait for the followers to signal.
-    runenv.recordMessage(`waiting for ${numFollowers} instances to become ready`)
-    const b = await client.barrier(readyState, numFollowers)
-    await b.wait
 
-    runenv.recordMessage('the followers are all ready')
-    runenv.recordMessage('ready...')
-    await sleep(1000) // 1s
-    runenv.recordMessage('set...')
-    await sleep(5000) // 5s
-    runenv.recordMessage('go, release followers!')
-
-    // signal on the 'released' state.
-    await client.signalEntry(releasedState)
-    return
-  }
-
-  const ms = getRandom(1, 5) * 1000
-  runenv.recordMessage(`i am a follower; signalling ready after ${ms} milliseconds`)
-
-  await sleep(ms)
-
-  runenv.recordMessage('follower signalling now')
-
-  // signal entry in the 'ready' state.
-  await client.signalEntry(readyState)
-
-  // wait until the leader releases us.
-  const b = await client.barrier(releasedState, 1)
-  await b.wait
-
-  runenv.recordMessage('i have been released')
+	// 🚧  Now we instantiate the ping service.
+	//
+	// This adds a stream handler to our Host so it can process inbound pings,
+	// and the returned PingService instance allows us to perform outbound pings.
+  const ping = new PingService()
+  // TODO: ^ how to construct this
 }
