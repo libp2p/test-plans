1fc969f35bb10ceb73cee88cacf92cd8be7fb081
Marco Munizaga <git@marcopolo.io>
2022-12-30 15:25:07 -0800

    Fix race condition in go ping

--- ping/go/compat/libp2p.v0.20.go
+++ ping/go/compat/libp2p.v0.20.go
@@ -9,8 +9,10 @@ import (
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p/config"
+	"github.com/libp2p/go-libp2p/core/event"
 
 	noise "github.com/libp2p/go-libp2p/p2p/security/noise"
 	tls "github.com/libp2p/go-libp2p/p2p/security/tls"
@@ -34,4 +36,32 @@ func getSecurityByName(secureChannel string) libp2p.Option {
 		return libp2p.Security(tls.ID, tls.New)
 	}
 	panic(fmt.Sprintf("unknown secure channel: %s", secureChannel))
+}
+
+type ConnEventsSub struct {
+	sub event.Subscription
+}
+
+func SubscribeToConnectedEvents() (ConnEventsSub, error) {
+	sub, err := host.EventBus().Subscribe(new(event.EvtPeerConnectednessChanged))
+	if err != nil {
+		return ConnEventsSub{}, err
+	}
+
+	return ConnEventsSub{
+		sub: sub,
+	}, nil
+
+}
+
+func (s *ConnEventsSub) WaitForNConnectedEvents(n int) {
+	connectedPeers := 0
+	for e := range s.sub.Out() {
+		if e.(event.EvtPeerConnectednessChanged).Connectedness == network.Connected {
+			connectedPeers++
+		}
+		if connectedPeers == n {
+			return
+		}
+	}
 }
--- ping/go/compat/libp2p.v0.22.go
+++ ping/go/compat/libp2p.v0.22.go
@@ -8,7 +8,9 @@ import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p/config"
+	"github.com/libp2p/go-libp2p/core/event"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 
@@ -24,6 +26,34 @@ func NewLibp2(ctx context.Context, secureChannel string, opts ...config.Option) 
 	return libp2p.New(
 		append(opts, security)...,
 	)
+}
+
+type ConnEventsSub struct {
+	sub event.Subscription
+}
+
+func SubscribeToConnectedEvents() (ConnEventsSub, error) {
+	sub, err := host.EventBus().Subscribe(new(event.EvtPeerConnectednessChanged))
+	if err != nil {
+		return ConnEventsSub{}, err
+	}
+
+	return ConnEventsSub{
+		sub: sub,
+	}, nil
+
+}
+
+func (s *ConnEventsSub) WaitForNConnectedEvents(n int) {
+	connectedPeers := 0
+	for e := range s.sub.Out() {
+		if e.(event.EvtPeerConnectednessChanged).Connectedness == network.Connected {
+			connectedPeers++
+		}
+		if connectedPeers == n {
+			return
+		}
+	}
 }
 
 func getSecurityByName(secureChannel string) libp2p.Option {
