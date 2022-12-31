7c0afd0cd14b231f65a7719fc2c978b05d6543bf
Marco Munizaga <git@marcopolo.io>
2022-12-30 16:57:20 -0800

    Fix import

--- ping/go/compat/libp2p.v0.22.go
+++ ping/go/compat/libp2p.v0.22.go
@@ -8,10 +8,10 @@ import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p/config"
 	"github.com/libp2p/go-libp2p/core/event"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 
 	noise "github.com/libp2p/go-libp2p/p2p/security/noise"
