a644753f6257c075a779d11e7c61ed7c67adfc4b
Marco Munizaga <git@marcopolo.io>
2023-01-08 16:12:42 -0800

    Support running interop workflow from other repos

--- multidim-interop/testplans.ts
+++ multidim-interop/testplans.ts
@@ -1,12 +1,66 @@
 import { buildTestSpecs } from "./src/generator"
-import { versions } from './versions'
-import { promises as fs } from 'fs';
+import { Version, versions } from "./versions"
+import { promises as fs } from "fs";
 import { run, RunFailure } from "./src/compose-runner"
 import { stringify } from "csv-stringify/sync"
+import { stringify as YAMLStringify } from "yaml"
+import yargs from "yargs/yargs"
+import path from "path";
 
-const WorkerCount = parseInt(process.env.WORKER_COUNT || "1")
+(async () => {
+    const WorkerCount = parseInt(process.env.WORKER_COUNT || "1")
+    const argv = await yargs(process.argv.slice(2))
+        .options({
+            'name-filter': {
+                description: 'Only run named test',
+                default: "",
+            },
+            'emit-only': {
+                alias: 'e',
+                description: 'Only print the compose.yaml file',
+                default: false,
+                type: 'boolean'
+            },
+            'extra-versions-dir': {
+                description: 'Look for extra versions in this directory. Version files must be in json format',
+                default: "",
+                type: 'string'
+            },
+        })
+        .help()
+        .version(false)
+        .alias('help', 'h').argv;
+    const extraVersionsDir = argv.extraVersionsDir
+    const extraVersions: Array<Version> = []
+    if (extraVersionsDir !== "") {
+        try {
+            const files = await fs.readdir(extraVersionsDir);
+            for (const file of files) {
+                const contents = await fs.readFile(path.join(extraVersionsDir, file))
+                extraVersions.push(...JSON.parse(contents.toString()))
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
-buildTestSpecs(versions).then(async (testSpecs) => {
+    let testSpecs = await buildTestSpecs(versions.concat(extraVersions))
+
+    const nameFilter = argv["name-filter"]
+    if (nameFilter !== "") {
+        testSpecs = testSpecs.filter((testSpec) => testSpec.name?.includes(nameFilter))
+    }
+
+
+    if (argv["emit-only"]) {
+        for (const testSpec of testSpecs) {
+            console.log("## " + testSpec.name)
+            console.log(YAMLStringify(testSpec))
+            console.log("\n\n")
+        }
+        return
+    }
+
     console.log(`Running ${testSpecs.length} tests`)
     const failures: Array<RunFailure> = []
     const statuses: Array<string[]> = [["name", "outcome"]]
@@ -31,4 +85,5 @@ buildTestSpecs(versions).then(async (testSpecs) => {
     console.log(`${failures.length} failures`, failures)
     await fs.writeFile("results.csv", stringify(statuses))
 
-}).then(() => console.log("Run complete"))
+    console.log("Run complete")
+})()
