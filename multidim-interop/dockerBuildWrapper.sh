59b5d90934996e0e58fbe093b6fc3f2a39e625b5
Marco Munizaga <git@marcopolo.io>
2023-02-08 16:47:05 -0800

    Use composite action and S3 caching

--- multidim-interop/dockerBuildWrapper.sh
+++ multidim-interop/dockerBuildWrapper.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env /bin/bash
 
-# If in CI use caching
-if [[ -n "${CI}" ]]; then
+# If in CI and we have a defined cache bucket, use caching
+if [[ -n "${CI}" ]] && [[ -n "${AWS_BUCKET}" ]]; then
     docker buildx build \
     --load \
     -t $IMAGE_NAME \
-    --cache-to type=gha,mode=max,scope=$GITHUB_REF_NAME-$IMAGE_NAME \
-    --cache-from type=gha,scope=$GITHUB_REF_NAME-$IMAGE_NAME "$@"
+    --cache-to   type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME \
+    --cache-from type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME \
+    "$@"
 else
 	docker build -t $IMAGE_NAME "$@"
 fi
