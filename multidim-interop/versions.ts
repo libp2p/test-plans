9d6ef1daadc2aa7cb900d8e3e0bef69d5b1afc83
Tanguy <tanguy@status.im>
2023-02-15 14:46:36 +0100

    fix

--- multidim-interop/versions.ts
+++ multidim-interop/versions.ts
@@ -4,7 +4,7 @@ import gov023 from "./go/v0.23/image.json"
 import gov022 from "./go/v0.22/image.json"
 import rustv050 from "./rust/v0.50/image.json"
 import jsV041 from "./js/v0.41/node-image.json"
-import nimv10 from "./nim/v1.0/node-image.json"
+import nimv10 from "./nim/v1.0/image.json"
 import chromiumJsV041 from "./js/v0.41/chromium-image.json"
 
 export type Version = {
