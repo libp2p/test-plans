f549b4359495eab8cc4eba11aacf786a4efe43a7
Marco Munizaga <git@marcopolo.io>
2023-03-03 20:15:41 -0800

    node-image.json

--- multidim-interop/versions.ts
+++ multidim-interop/versions.ts
@@ -6,6 +6,7 @@ import rustv048 from "./rust/v0.48/image.json"
 import rustv049 from "./rust/v0.49/image.json"
 import rustv050 from "./rust/v0.50/image.json"
 import jsV041 from "./js/v0.41/node-image.json"
+import jsV042 from "./js/v0.42/node-image.json"
 import nimv10 from "./nim/v1.0/image.json"
 import chromiumJsV041 from "./js/v0.41/chromium-image.json"
 import chromiumJsV042 from "./js/v0.42/chromium-image.json"
@@ -45,6 +46,13 @@ export const versions: Array<Version> = [
     {
         id: "js-v0.41.0",
         containerImageID: jsV041.imageID,
+        transports: ["tcp", "ws"],
+        secureChannels: ["noise"],
+        muxers: ["mplex", "yamux"],
+    },
+    {
+        id: "js-v0.42.0",
+        containerImageID: jsV042.imageID,
         transports: ["tcp", "ws"],
         secureChannels: ["noise"],
         muxers: ["mplex", "yamux"],
