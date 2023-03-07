d677d63632b0f4b4e31d17387b085540953a5db7
Max Inden <mail@max-inden.de>
2023-03-07 10:51:06 +0100

    Replace transport mux and sec dimension with role

--- relay/src/generator.ts
+++ relay/src/generator.ts
@@ -26,21 +26,13 @@ export async function buildTestSpecs(versions: Array<Version>): Promise<Array<Co
         driver: sqlite3.Database,
     });
 
-    await db.exec(`CREATE TABLE IF NOT EXISTS transports (id string not null, transport string not null, onlyDial boolean not null);`)
-    await db.exec(`CREATE TABLE IF NOT EXISTS secureChannels (id string not null, sec string not null);`)
-    await db.exec(`CREATE TABLE IF NOT EXISTS muxers (id string not null, muxer string not null);`)
+    await db.exec(`CREATE TABLE IF NOT EXISTS roles (id string not null, role string not null);`)
 
     await Promise.all(
         versions.flatMap(version => {
             return [
-                db.exec(`INSERT INTO transports (id, transport, onlyDial)
-                VALUES ${version.transports.map(normalizeTransport).map(transport => `("${version.id}", "${transport.name}", ${transport.onlyDial})`).join(", ")};`),
-                (version.secureChannels.length > 0 ?
-                    db.exec(`INSERT INTO secureChannels (id, sec)
-                VALUES ${version.secureChannels.map(sec => `("${version.id}", "${sec}")`).join(", ")};`) : []),
-                (version.muxers.length > 0 ?
-                    db.exec(`INSERT INTO muxers (id, muxer)
-                VALUES ${version.muxers.map(muxer => `("${version.id}", "${muxer}")`).join(", ")};`) : []),
+                db.exec(`INSERT INTO roles (id, role)
+                VALUES ${version.roles.map(role => `("${version.id}", "${role}")`).join(", ")};`),
             ]
         })
     )
@@ -49,115 +41,51 @@ export async function buildTestSpecs(versions: Array<Version>): Promise<Array<Co
     // and muxers tables the distinct combinations where the transport and the muxer
     // of the different libp2p implementations match.
     const queryResults =
-        await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport, ma.muxer, sa.sec
-                     FROM transports a, transports b, muxers ma, muxers mb, secureChannels sa, secureChannels sb
-                     WHERE a.id == ma.id
-                     AND NOT b.onlyDial
-                     AND b.id == mb.id
-                     AND a.id == sa.id
-                     AND b.id == sb.id
-                     AND a.transport == b.transport
-                     AND sa.sec == sb.sec
-                     AND ma.muxer == mb.muxer
-                     -- quic only uses its own muxer/securechannel
-                     AND a.transport != "webtransport"
-                     AND a.transport != "webrtc"
-                     AND a.transport != "quic"
-                     AND a.transport != "quic-v1";`);
-    const quicQueryResults =
-        await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport
-                     FROM transports a, transports b
-                     WHERE a.transport == b.transport
-                     AND NOT b.onlyDial
-                     -- Only quic transports
-                     AND a.transport == "quic";`);
-    const quicV1QueryResults =
-        await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport
-                     FROM transports a, transports b
-                     WHERE a.transport == b.transport
-                     AND NOT b.onlyDial
-                     -- Only quic transports
-                     AND a.transport == "quic-v1";`);
-    const webtransportQueryResults =
-        await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport
-                     FROM transports a, transports b
-                     WHERE a.transport == b.transport
-                     AND NOT b.onlyDial
-                     -- Only webtransport transports
-                     AND a.transport == "webtransport";`);
-    const webrtcQueryResults =
-        await db.all(`SELECT DISTINCT a.id as id1, b.id as id2, a.transport
-                     FROM transports a, transports b
-                     WHERE a.transport == b.transport
-                     AND NOT b.onlyDial
-                     -- Only webrtc transports
-                     AND a.transport == "webrtc";`);
+        await db.all(`SELECT DISTINCT src.id as idsrc, relay.id as idrelay, dst.id as iddst
+                     FROM roles src, roles relay, roles dst;`);
     await db.close();
 
     const testSpecs = queryResults.map((test): ComposeSpecification => (
         buildSpec(containerImages, {
-            name: `${test.id1} x ${test.id2} (${test.transport}, ${test.sec}, ${test.muxer})`,
-            dialerID: test.id1,
-            listenerID: test.id2,
-            transport: test.transport,
-            muxer: test.muxer,
-            security: test.sec,
-            extraEnv: buildExtraEnv(timeoutOverride, test.id1, test.id2)
+            name: `${test.idsrc} <-> ${test.idrelay} <-> ${test.iddst}`,
+            sourceID: test.idsrc,
+            relayID: test.idrelay,
+            destinationID: test.iddst,
         })
-    )).concat(
-        quicQueryResults
-            .concat(quicV1QueryResults)
-            .concat(webtransportQueryResults)
-            .map((test): ComposeSpecification => buildSpec(containerImages, {
-                name: `${test.id1} x ${test.id2} (${test.transport})`,
-                dialerID: test.id1,
-                listenerID: test.id2,
-                transport: test.transport,
-                extraEnv: buildExtraEnv(timeoutOverride, test.id1, test.id2)
-            })))
-        .concat(webrtcQueryResults
-            .map((test): ComposeSpecification => buildSpec(containerImages, {
-                name: `${test.id1} x ${test.id2} (${test.transport})`,
-                dialerID: test.id1,
-                listenerID: test.id2,
-                transport: test.transport,
-                extraEnv: buildExtraEnv(timeoutOverride, test.id1, test.id2)
-            })))
+    ))
 
     return testSpecs
 }
 
-function buildSpec(containerImages: { [key: string]: string }, { name, dialerID, listenerID, transport, muxer, security, extraEnv }: { name: string, dialerID: string, listenerID: string, transport: string, muxer?: string, security?: string, extraEnv?: { [key: string]: string } }): ComposeSpecification {
+function buildSpec(containerImages: { [key: string]: string }, { name, sourceID, relayID, destinationID }: { name: string, sourceID: string, relayID: string, destinationID: string }): ComposeSpecification {
     return {
         name,
         services: {
-            dialer: {
-                image: containerImages[dialerID],
+            source: {
+                init: true,
+                image: containerImages[sourceID],
                 depends_on: ["redis"],
                 environment: {
-                    version: dialerID,
-                    transport,
-                    is_dialer: true,
-                    ip: "0.0.0.0",
-                    ...(!!muxer && { muxer }),
-                    ...(!!security && { security }),
-                    ...extraEnv,
+                    role: "source",
+                    version: sourceID,
                 }
             },
-            listener: {
-                // Add init process to be PID 1 to proxy signals. Rust doesn't
-                // handle SIGINT without this
+            relay: {
                 init: true,
-                image: containerImages[listenerID],
+                image: containerImages[relayID],
                 depends_on: ["redis"],
                 environment: {
-                    version: listenerID,
-                    transport,
-                    is_dialer: false,
-                    ip: "0.0.0.0",
-                    ...(!!muxer && { muxer }),
-                    ...(!!security && { security }),
-                    ...extraEnv,
+                    role: "relay",
+                    version: relayID,
+                }
+            },
+            destination: {
+                init: true,
+                image: containerImages[destinationID],
+                depends_on: ["redis"],
+                environment: {
+                    role: "destination",
+                    version: destinationID,
                 }
             },
             redis: {
@@ -168,8 +96,4 @@ function buildSpec(containerImages: { [key: string]: string }, { name, dialerID,
             }
         }
     }
-}
-
-function normalizeTransport(transport: string | { name: string, onlyDial: boolean }): { name: string, onlyDial: boolean } {
-    return typeof transport === "string" ? { name: transport, onlyDial: false } : transport
 }
--- relay/versions.ts
+++ relay/versions.ts
@@ -1,101 +1,17 @@
-// import gov025 from "./go/v0.25/image.json"
-// import gov024 from "./go/v0.24/image.json"
-// import gov023 from "./go/v0.23/image.json"
-// import gov022 from "./go/v0.22/image.json"
-// import rustv048 from "./rust/v0.48/image.json"
-// import rustv049 from "./rust/v0.49/image.json"
-// import rustv050 from "./rust/v0.50/image.json"
 import rustv051 from "./rust/v0.51/image.json"
-// import jsV041 from "./js/v0.41/node-image.json"
-// import nimv10 from "./nim/v1.0/image.json"
-// import chromiumJsV041 from "./js/v0.41/chromium-image.json"
 
 export type Version = {
     id: string,
     containerImageID: string,
     // If defined, this will increase the timeout for tests using this version
     timeoutSecs?: number,
-    transports: Array<(string | { name: string, onlyDial: boolean })>,
-    secureChannels: string[],
-    muxers: string[]
+    roles: string[]
 }
 
 export const versions: Array<Version> = [
-    // {
-    //     id: "rust-v0.48.0",
-    //     containerImageID: rustv048.imageID,
-    //     transports: ["ws", "tcp"],
-    //     secureChannels: ["noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "rust-v0.49.0",
-    //     containerImageID: rustv049.imageID,
-    //     transports: ["ws", "tcp"],
-    //     secureChannels: ["noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "rust-v0.50.0",
-    //     containerImageID: rustv050.imageID,
-    //     transports: ["ws", "tcp", "quic-v1", "webrtc"],
-    //     secureChannels: ["tls", "noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
     {
         id: "rust-v0.51.0",
         containerImageID: rustv051.imageID,
-        transports: ["ws", "tcp", "quic-v1", "webrtc"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
+        roles: ["source", "relay", "destination"],
     },
-    // {
-    //     id: "js-v0.41.0",
-    //     containerImageID: jsV041.imageID,
-    //     transports: ["tcp", "ws"],
-    //     secureChannels: ["noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "chromium-js-v0.41.0",
-    //     containerImageID: chromiumJsV041.imageID,
-    //     transports: [{ name: "webtransport", onlyDial: true }, { name: "webrtc", onlyDial: true }],
-    //     secureChannels: [],
-    //     muxers: []
-    // },
-    // {
-    //     id: "go-v0.25.1",
-    //     containerImageID: gov025.imageID,
-    //     transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
-    //     secureChannels: ["tls", "noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "go-v0.24.2",
-    //     containerImageID: gov024.imageID,
-    //     transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
-    //     secureChannels: ["tls", "noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "go-v0.23.4",
-    //     containerImageID: gov023.imageID,
-    //     transports: ["tcp", "ws", "quic"],
-    //     secureChannels: ["tls", "noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "go-v0.22.0",
-    //     containerImageID: gov022.imageID,
-    //     transports: ["tcp", "ws", "quic"],
-    //     secureChannels: ["tls", "noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
-    // {
-    //     id: "nim-v1.0",
-    //     containerImageID: nimv10.imageID,
-    //     transports: ["tcp", "ws"],
-    //     secureChannels: ["noise"],
-    //     muxers: ["mplex", "yamux"],
-    // },
 ]
