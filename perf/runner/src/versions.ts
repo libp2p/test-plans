e0fa1f403ef824227f0d665e72e48b4e222a10c4
Max Inden <mail@max-inden.de>
2023-04-12 19:26:27 +0200

    Update to latest container images

--- perf/runner/src/versions.ts
+++ perf/runner/src/versions.ts
@@ -7,12 +7,12 @@ export type Version = {
 export const versions: Array<Version> = [
     {
         id: "rust-master",
-        containerImageID: "f75472c773218b4da229e8b83c498d67de70cf2c6d145a969ca31aea14b78962",
+        containerImageID: "f567b27347a8d222e88f3c0b160e0547023e88aa700dc5e4255d9fcdf3d08eb1",
         transportStacks: ["tcp", "quic-v1"]
     },
     {
         id: "go-master",
-        containerImageID: "9eaea3b70d5895d92219df971008eb1e55b8de769d1195cc03e2d74b314d6eac",
+        containerImageID: "2406b0778d7655b7eac7c0b448b1bf008c2411b7f128e899d894dcc12abb98fd",
         transportStacks: ["tcp", "quic-v1"]
     },
 ]
