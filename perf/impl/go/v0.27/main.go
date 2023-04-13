8229e2bdbc73f0fa0415407d866f1b130c25d098
Max Inden <mail@max-inden.de>
2023-04-13 17:57:11 +0200

    Seed Go secret key from command line and fix server invocation

--- perf/impl/go/v0.27/main.go
+++ perf/impl/go/v0.27/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/multiformats/go-multiaddr"
 )
 
@@ -16,6 +17,8 @@ func main() {
 	runServer := flag.Bool("run-server", false, "Should run as server")
 	// --server-address <SERVER_ADDRESS>
 	serverAddr := flag.String("server-address", "", "Server address")
+	// --secret-key-seed <SEED>
+	secretKeySeed := flag.Uint64("secret-key-seed", 0, "Server secret key seed")
 	// --upload-bytes <UPLOAD_BYTES>
 	uploadBytes := flag.Int("upload-bytes", 0, "Upload bytes")
 	// --download-bytes <DOWNLOAD_BYTES>
@@ -24,12 +27,20 @@ func main() {
 	nTimes := flag.Int("n-times", 0, "N times")
 	flag.Parse()
 
-	// var opts []libp2p.Option
-	// if *runServer != "" {
-	// 	opts = append(opts, libp2p.ListenAddrStrings(*runServer))
-	// }
 
-	h, err := libp2p.New()
+	var opts []libp2p.Option
+	if *runServer  {
+		opts = append(opts, libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/udp/4001/quic-v1"))
+
+		// TODO: Fake identity. For testing only.
+		priv, _, err := crypto.GenerateEd25519Key(&simpleReader{seed:uint8(*secretKeySeed)})
+		if err != nil {
+			panic(err)
+		}
+		opts = append(opts, libp2p.Identity(priv))
+	}
+
+	h, err := libp2p.New(opts...)
 	if err != nil {
 		panic(err)
 	}
@@ -84,4 +95,15 @@ func main() {
 
 type Latencies struct {
 	Latencies []float32 `json:"latencies"`
+}
+
+type simpleReader struct {
+	seed uint8
+}
+
+func (r *simpleReader) Read(p []byte) (n int, err error) {
+	for i := range p {
+		p[i] = r.seed
+	}
+	return len(p), nil
 }
