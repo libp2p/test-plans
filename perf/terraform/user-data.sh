24333385107a7bc7b6fb1df833bb5be8e6fbf7c8
Max Inden <mail@max-inden.de>
2023-04-13 18:07:02 +0200

    Bump UDP receive buffer

--- perf/terraform/user-data.sh
+++ perf/terraform/user-data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Bump UDP receive buffer size. See https://github.com/quic-go/quic-go/wiki/UDP-Receive-Buffer-Size.
+sudo sysctl -w net.core.rmem_max=2500000
+
 sudo yum update -y
 sudo yum install docker -y
 sudo systemctl enable docker
