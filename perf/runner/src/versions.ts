d559bb3d8741cee62eb4064f29d9513bf6cc78ea
Max Inden <mail@max-inden.de>
2023-04-14 16:45:14 +0200

    Fix versions.ts adding sha256: to other images

--- perf/runner/src/versions.ts
+++ perf/runner/src/versions.ts
@@ -10,13 +10,13 @@ export const versions: Array<Version> = [
     {
         id: "rust-master",
         implementation: "rust-libp2p",
-        containerImageID: "mxinden/libp2p-perf@3078e6a9941952486afe4b417a318ce0c372b6e2ef1e296c85f7811c1f170b09",
+        containerImageID: "mxinden/libp2p-perf@sha256:3078e6a9941952486afe4b417a318ce0c372b6e2ef1e296c85f7811c1f170b09",
         transportStacks: ["tcp", "quic-v1"]
     },
     {
         id: "go-v0.27.0",
         implementation: "go-libp2p",
-        containerImageID: "mxinden/libp2p-perf@0fb68df50eada9dd10058bd7b6da614f2a31148dc0343be8eb5c7920f7bb4eef",
+        containerImageID: "mxinden/libp2p-perf@sha256:0fb68df50eada9dd10058bd7b6da614f2a31148dc0343be8eb5c7920f7bb4eef",
         transportStacks: ["tcp", "quic-v1"]
     },
     {
@@ -29,7 +29,7 @@ export const versions: Array<Version> = [
     {
         id: "go-https-v0.0.1",
         implementation: "go-https",
-        containerImageID: "ca9237cdbdfb13ac68f019bc1373ccfcc207798e461f8e9264d52f981d9d648c",
+        containerImageID: "mxinden/libp2p-perf@sha256:ca9237cdbdfb13ac68f019bc1373ccfcc207798e461f8e9264d52f981d9d648c",
         transportStacks: ["tcp"]
     },
 ]
