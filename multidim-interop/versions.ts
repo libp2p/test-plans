6caca053b61cc489bca9bfd5be2e242f25b18848
Thomas Eizinger <thomas@eizinger.io>
2023-05-04 20:36:33 +0200

    Setup reproducable test failure

--- multidim-interop/versions.ts
+++ multidim-interop/versions.ts
@@ -1,17 +1,19 @@
-import gov025 from "./impl/go/v0.25/image.json"
-import gov024 from "./impl/go/v0.24/image.json"
-import gov023 from "./impl/go/v0.23/image.json"
-import gov022 from "./impl/go/v0.22/image.json"
-import rustv048 from "./impl/rust/v0.48/image.json"
-import rustv049 from "./impl/rust/v0.49/image.json"
-import rustv050 from "./impl/rust/v0.50/image.json"
-import rustv051 from "./impl/rust/v0.51/image.json"
-import jsV041 from "./impl/js/v0.41/node-image.json"
-import jsV042 from "./impl/js/v0.42/node-image.json"
-import nimv10 from "./impl/nim/v1.0/image.json"
-import chromiumJsV041 from "./impl/js/v0.41/chromium-image.json"
-import chromiumJsV042 from "./impl/js/v0.42/chromium-image.json"
-import zigv001 from "./impl/zig/v0.0.1/image.json"
+// import gov025 from "./impl/go/v0.25/image.json"
+// import gov024 from "./impl/go/v0.24/image.json"
+// import gov023 from "./impl/go/v0.23/image.json"
+// import gov022 from "./impl/go/v0.22/image.json"
+// import rustv048 from "./impl/rust/v0.48/image.json"
+// import rustv049 from "./impl/rust/v0.49/image.json"
+// import rustv050 from "./impl/rust/v0.50/image.json"
+// import rustv051 from "./impl/rust/v0.51/image.json"
+import rustBumpYamux from "./impl/rust/bump-yamux/image.json"
+// import jsV041 from "./impl/js/v0.41/node-image.json"
+// import jsV042 from "./impl/js/v0.42/node-image.json"
+import jsHead from "./impl/js/head/node-image.json"
+// import nimv10 from "./impl/nim/v1.0/image.json"
+// import chromiumJsV041 from "./impl/js/v0.41/chromium-image.json"
+// import chromiumJsV042 from "./impl/js/v0.42/chromium-image.json"
+// import zigv001 from "./impl/zig/v0.0.1/image.json"
 
 export type Version = {
     id: string,
@@ -25,101 +27,115 @@ export type Version = {
 
 export const versions: Array<Version> = [
     {
-        id: "rust-v0.48.0",
-        containerImageID: rustv048.imageID,
-        transports: ["ws", "tcp"],
+        id: "rust-bump-yamux",
+        containerImageID: rustBumpYamux.imageID,
+        transports: ["tcp"],
         secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
+        muxers: ["yamux"],
     },
     {
-        id: "rust-v0.49.0",
-        containerImageID: rustv049.imageID,
-        transports: ["ws", "tcp"],
+        id: "js-head",
+        containerImageID: jsHead.imageID,
+        transports: ["tcp"],
         secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
+        muxers: ["yamux"],
     },
-    {
-        id: "rust-v0.50.0",
-        containerImageID: rustv050.imageID,
-        transports: ["ws", "tcp", "quic-v1"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "rust-v0.51.0",
-        containerImageID: rustv051.imageID,
-        transports: ["ws", "tcp", "quic-v1", "webrtc-direct"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "js-v0.41.0",
-        containerImageID: jsV041.imageID,
-        transports: ["tcp", "ws"],
-        secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "js-v0.42.0",
-        containerImageID: jsV042.imageID,
-        transports: ["tcp", "ws", { name: "wss", onlyDial: true }],
-        secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "chromium-js-v0.41.0",
-        containerImageID: chromiumJsV041.imageID,
-        transports: [{ name: "webtransport", onlyDial: true }],
-        secureChannels: [],
-        muxers: []
-    },
-    {
-        id: "chromium-js-v0.42.0",
-        containerImageID: chromiumJsV042.imageID,
-        transports: [{ name: "webtransport", onlyDial: true }, { name: "wss", onlyDial: true }],
-        secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"]
-    },
-    {
-        id: "go-v0.25.1",
-        containerImageID: gov025.imageID,
-        transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "go-v0.24.2",
-        containerImageID: gov024.imageID,
-        transports: ["tcp", "ws", "quic", "quic-v1", "webtransport", "wss"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "go-v0.23.4",
-        containerImageID: gov023.imageID,
-        transports: ["tcp", "ws", "quic"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "go-v0.22.0",
-        containerImageID: gov022.imageID,
-        transports: ["tcp", "ws", "quic"],
-        secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "nim-v1.0",
-        containerImageID: nimv10.imageID,
-        transports: ["tcp", "ws"],
-        secureChannels: ["noise"],
-        muxers: ["mplex", "yamux"],
-    },
-    {
-        id: "zig-v0.0.1",
-        containerImageID: zigv001.imageID,
-        transports: ["quic-v1"],
-        secureChannels: [],
-        muxers: [],
-    },
+    // {
+    //     id: "rust-v0.48.0",
+    //     containerImageID: rustv048.imageID,
+    //     transports: ["ws", "tcp"],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "rust-v0.49.0",
+    //     containerImageID: rustv049.imageID,
+    //     transports: ["ws", "tcp"],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "rust-v0.50.0",
+    //     containerImageID: rustv050.imageID,
+    //     transports: ["ws", "tcp", "quic-v1"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "rust-v0.51.0",
+    //     containerImageID: rustv051.imageID,
+    //     transports: ["ws", "tcp", "quic-v1", "webrtc-direct"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "js-v0.41.0",
+    //     containerImageID: jsV041.imageID,
+    //     transports: ["tcp", "ws"],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "js-v0.42.0",
+    //     containerImageID: jsV042.imageID,
+    //     transports: ["tcp", "ws", { name: "wss", onlyDial: true }],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "chromium-js-v0.41.0",
+    //     containerImageID: chromiumJsV041.imageID,
+    //     transports: [{ name: "webtransport", onlyDial: true }],
+    //     secureChannels: [],
+    //     muxers: []
+    // },
+    // {
+    //     id: "chromium-js-v0.42.0",
+    //     containerImageID: chromiumJsV042.imageID,
+    //     transports: [{ name: "webtransport", onlyDial: true }, { name: "wss", onlyDial: true }],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"]
+    // },
+    // {
+    //     id: "go-v0.25.1",
+    //     containerImageID: gov025.imageID,
+    //     transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "go-v0.24.2",
+    //     containerImageID: gov024.imageID,
+    //     transports: ["tcp", "ws", "quic", "quic-v1", "webtransport", "wss"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "go-v0.23.4",
+    //     containerImageID: gov023.imageID,
+    //     transports: ["tcp", "ws", "quic"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "go-v0.22.0",
+    //     containerImageID: gov022.imageID,
+    //     transports: ["tcp", "ws", "quic"],
+    //     secureChannels: ["tls", "noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "nim-v1.0",
+    //     containerImageID: nimv10.imageID,
+    //     transports: ["tcp", "ws"],
+    //     secureChannels: ["noise"],
+    //     muxers: ["mplex", "yamux"],
+    // },
+    // {
+    //     id: "zig-v0.0.1",
+    //     containerImageID: zigv001.imageID,
+    //     transports: ["quic-v1"],
+    //     secureChannels: [],
+    //     muxers: [],
+    // },
 ]
