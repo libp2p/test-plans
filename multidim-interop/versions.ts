06a82c0d95612ffdc03ad08be46b0a91ab5e1507
Marco Munizaga <marco@marcopolo.io>
2023-06-08 14:56:17 -0700

    Reintroduce js-libp2p v0.45.0 and fix build from cache (#189)
    * Add js-libp2p v0.45.0
    
    * Name the image before creating browser images
    
    * Update commit sha to test webrtc
    
    * Enable webrtc & webrtc-direct tests

--- multidim-interop/versions.ts
+++ multidim-interop/versions.ts
@@ -11,10 +11,13 @@ import rustv051 from "./impl/rust/v0.51/image.json"
 import jsV041 from "./impl/js/v0.41/node-image.json"
 import jsV042 from "./impl/js/v0.42/node-image.json"
 import jsV044 from "./impl/js/v0.44/node-image.json"
+import jsV045 from "./impl/js/v0.45/image.json"
 import nimv10 from "./impl/nim/v1.0/image.json"
 import chromiumJsV041 from "./impl/js/v0.41/chromium-image.json"
 import chromiumJsV042 from "./impl/js/v0.42/chromium-image.json"
 import chromiumJsV044 from "./impl/js/v0.44/chromium-image.json"
+import chromiumJsV045 from "./impl/js/v0.45/chromium-image.json"
+import firefoxJsV045 from "./impl/js/v0.45/firefox-image.json"
 import zigv001 from "./impl/zig/v0.0.1/image.json"
 
 export type Version = {
@@ -78,6 +81,13 @@ export const versions: Array<Version> = [
         muxers: ["mplex", "yamux"],
     },
     {
+        id: "js-v0.45.0",
+        containerImageID: jsV045.imageID,
+        transports: ["tcp", "ws", { name: "wss", onlyDial: true }],
+        secureChannels: ["noise"],
+        muxers: ["mplex", "yamux"],
+    },
+    {
         id: "chromium-js-v0.41.0",
         containerImageID: chromiumJsV041.imageID,
         transports: [{ name: "webtransport", onlyDial: true }],
@@ -95,6 +105,20 @@ export const versions: Array<Version> = [
         id: "chromium-js-v0.44.0",
         containerImageID: chromiumJsV044.imageID,
         transports: [{ name: "webtransport", onlyDial: true }, { name: "wss", onlyDial: true }, { name: "webrtc-direct", onlyDial: true }],
+        secureChannels: ["noise"],
+        muxers: ["mplex", "yamux"],
+    },
+    {
+        id: "chromium-js-v0.45.0",
+        containerImageID: chromiumJsV045.imageID,
+        transports: [{ name: "webtransport", onlyDial: true }, { name: "wss", onlyDial: true }, { name: "webrtc-direct", onlyDial: true }, "webrtc"],
+        secureChannels: ["noise"],
+        muxers: ["mplex", "yamux"],
+    },
+    {
+        id: "firefox-js-v0.45.0",
+        containerImageID: firefoxJsV045.imageID,
+        transports: [{ name: "wss", onlyDial: true }, { name: "webrtc-direct", onlyDial: true }, "webrtc"],
         secureChannels: ["noise"],
         muxers: ["mplex", "yamux"],
     },
