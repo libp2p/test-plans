cfd1049e8aaba19e5f9e6d54997ca90f2364e068
Max Inden <mail@max-inden.de>
2023-08-04 10:23:47 +0200

    fix(perf): run iperf on TCP and set TCP windows to bdp (#241)
    This commit makes iperf use TCP instead of UDP. Using UDP allows the sender to
    send as much data as they can, not as much data as the receiver can handle.
    Using TCP measures the actual bandwidth of the connection.
    
    This commit sets the TCP send and receive window to the bandwidth-delay-product.
    In order to exhaust the whole bandwidth between the two nodes, TCP needs to be
    able to buffer all bytes on the wire in the case of packet loss.

--- perf/terraform/modules/long_lived/files/user-data.sh
+++ perf/terraform/modules/long_lived/files/user-data.sh
@@ -7,6 +7,14 @@ sudo yum -y install iperf3
 # Bump UDP receive buffer size. See https://github.com/quic-go/quic-go/wiki/UDP-Receive-Buffer-Size.
 sudo sysctl -w net.core.rmem_max=2500000
 
+# Set maximum TCP send and receive window to bandwidth-delay-product.
+#
+# With a bandwidth of 25 Gbit/s per machine and a ping of 60 ms between the two
+# machines, the bandwidth-delay-product is ~178.81 MiB. Set send and receive
+# window to 200 MiB.
+sudo sysctl -w net.ipv4.tcp_rmem='4096 131072 200000000'
+sudo sysctl -w net.ipv4.tcp_wmem='4096 20480 200000000'
+
 sudo yum update -y
 sudo yum install docker -y
 sudo systemctl enable docker
