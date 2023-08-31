32545b9f0685e1090942b33a3ba8a534dad2a295
Prithvi Shahi <shahi.prithvi@gmail.com>
2023-08-30 18:09:49 -0700

    chore: add go v0.31, rm v0.29

--- multidim-interop/versions.ts
+++ multidim-interop/versions.ts
@@ -96,16 +96,16 @@ export const versions: Array<Version> = [
         muxers: ["mplex", "yamux"],
     },
     {
-        id: "go-v0.30",
+        id: "go-v0.31",
         transports: ["tcp", "ws", "quic-v1", "webtransport"],
         secureChannels: ["tls", "noise"],
         muxers: ["yamux"],
     },
     {
-        id: "go-v0.29",
-        transports: ["tcp", "ws", "quic", "quic-v1", "webtransport"],
+        id: "go-v0.30",
+        transports: ["tcp", "ws", "quic-v1", "webtransport"],
         secureChannels: ["tls", "noise"],
-        muxers: ["mplex", "yamux"],
+        muxers: ["yamux"],
     },
     {
         id: "nim-v1.0",
