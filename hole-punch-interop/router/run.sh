8ac871d3b25a8d7b74aae3dfc871f90c9f340c0f
Thomas Eizinger <thomas@eizinger.io>
2023-09-15 13:08:32 +1000

    Wait for interfaces in router to be up and running

--- hole-punch-interop/router/run.sh
+++ hole-punch-interop/router/run.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Wait for interfaces to be up and running ...
+while ! ip addr show eth0; do sleep 1; done
+while ! ip addr show eth1; do sleep 1; done
+
 ADDR_EXTERNAL=$(ip -json addr show eth1 | jq '.[0].addr_info[0].local' -r)
 SUBNET_INTERNAL=$(ip -json addr show eth0 | jq '.[0].addr_info[0].local + "/" + (.[0].addr_info[0].prefixlen | tostring)' -r)
 
@@ -12,5 +16,3 @@ nft add rule ip nat postrouting ip saddr $SUBNET_INTERNAL oifname "eth1" snat $A
 tc qdisc add dev eth1 root netem delay 100ms
 
 tail -f /dev/null
-
-# tcpdump -i eth1
