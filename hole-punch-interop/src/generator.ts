1e37b93e939c75241dba726a5cb3be1cbb7a88a9
Thomas Eizinger <thomas@eizinger.io>
2023-10-17 10:07:25 +1100

    feat(hole-punch): add hole-punch interoperability test suite (#304)

---
+++ hole-punch-interop/src/generator.ts
@@ -0,0 +1,172 @@
+import sqlite3 from "sqlite3";
+import {open} from "sqlite";
+import {Version} from "../versions";
+import {ComposeSpecification} from "../compose-spec/compose-spec";
+import {sanitizeComposeName} from "./lib";
+import path from "path";
+
+export async function buildTestSpecs(versions: Array<Version>, nameFilter: string | null, nameIgnore: string | null, routerImageId: string, relayImageId: string, routerDelay: number, relayDelay: number, assetDir: string): Promise<Array<ComposeSpecification>> {
+    sqlite3.verbose();
+
+    const db = await open({
+        // In memory DB. We don't persist this.
+        filename: ":memory:",
+        driver: sqlite3.Database,
+    });
+
+    await db.exec('CREATE TABLE IF NOT EXISTS transports (id string not null, imageID string not null, transport string not null);');
+
+    await Promise.all(
+        versions.flatMap(version => ([
+            db.exec(`INSERT INTO transports (id, imageID, transport) VALUES ${version.transports.map(transport => `("${version.id}", "${version.containerImageID}", "${transport}")`).join(", ")};`)
+        ]))
+    )
+
+    // Generate the testing combinations by SELECT'ing from transports tables the distinct combinations where the transports of the different libp2p implementations match.
+    const queryResults =
+        await db.all(`SELECT DISTINCT a.id as dialer, a.imageID as dialerImage, b.id as listener, b.imageID as listenerImage, a.transport
+                      FROM transports a,
+                           transports b
+                      WHERE a.transport == b.transport;`
+        );
+    await db.close();
+
+    return queryResults
+        .map(testCase => {
+            let name = `${testCase.dialer} x ${testCase.listener} (${testCase.transport})`;
+
+            if (nameFilter && !name.includes(nameFilter)) {
+                return null
+            }
+            if (nameIgnore && name.includes(nameIgnore)) {
+                return null
+            }
+
+            return buildSpec(name, testCase.dialerImage, testCase.listenerImage, routerImageId, relayImageId, testCase.transport, routerDelay, relayDelay, assetDir, {})
+        })
+        .filter(spec => spec !== null)
+}
+
+function buildSpec(name: string, dialerImage: string, listenerImage: string, routerImageId: string, relayImageId: string, transport: string, routerDelay: number, relayDelay: number, assetDir: string, extraEnv: { [key: string]: string }): ComposeSpecification {
+    let internetNetworkName = `${sanitizeComposeName(name)}_internet`
+
+    let startupScriptFn = (actor: "dialer" | "listener") => (`
+        set -ex;
+
+        ROUTER_IP=$$(dig +short ${actor}_router)
+        INTERNET_SUBNET=$$(curl --silent --unix-socket /var/run/docker.sock http://localhost/networks/${internetNetworkName} | jq -r '.IPAM.Config[0].Subnet')
+
+        ip route add $$INTERNET_SUBNET via $$ROUTER_IP dev eth0
+
+        tcpdump -i eth0 -w /tmp/${actor}.pcap &
+
+        sleep 2 # Let tcpdump start up
+
+        hole-punch-client
+    `);
+
+    let relayStartupScript = `
+        set -ex;
+
+        tc qdisc add dev eth0 root netem delay ${relayDelay}ms; # Add a delay to all relayed connections
+
+        /usr/bin/relay
+    `;
+
+    const dockerSocketVolume = "/var/run/docker.sock:/var/run/docker.sock";
+    const tcpDumpVolume = `${path.join(assetDir, sanitizeComposeName(name))}:/tmp:rw`;
+
+    return {
+        name,
+        services: {
+            relay: {
+                depends_on: ["redis"],
+                image: relayImageId,
+                init: true,
+                command: ["/bin/sh", "-c", relayStartupScript],
+                networks: {
+                    internet: {},
+                },
+                cap_add: ["NET_ADMIN"]
+            },
+            dialer_router: {
+                depends_on: ["redis"],
+                image: routerImageId,
+                init: true,
+                environment: {
+                    DELAY_MS: routerDelay
+                },
+                networks: {
+                    lan_dialer: {},
+                    internet: {},
+                },
+                cap_add: ["NET_ADMIN"],
+            },
+            dialer: {
+                depends_on: ["relay", "dialer_router", "redis"],
+                image: dialerImage,
+                init: true,
+                command: ["/bin/sh", "-c", startupScriptFn("dialer")],
+                environment: {
+                    TRANSPORT: transport,
+                    MODE: "dial",
+                },
+                networks: {
+                    lan_dialer: {},
+                },
+                cap_add: ["NET_ADMIN"],
+                volumes: [dockerSocketVolume, tcpDumpVolume]
+            },
+            listener_router: {
+                depends_on: ["redis"],
+                image: routerImageId,
+                init: true,
+                environment: {
+                    DELAY_MS: routerDelay
+                },
+                networks: {
+                    lan_listener: {},
+                    internet: {},
+                },
+                cap_add: ["NET_ADMIN"]
+            },
+            listener: {
+                depends_on: ["relay", "listener_router", "redis"],
+                image: listenerImage,
+                init: true,
+                command: ["/bin/sh", "-c", startupScriptFn("listener")],
+                environment: {
+                    TRANSPORT: transport,
+                    MODE: "listen",
+                },
+                networks: {
+                    lan_listener: {},
+                },
+                cap_add: ["NET_ADMIN"],
+                volumes: [dockerSocketVolume, tcpDumpVolume]
+            },
+            redis: {
+                image: "redis:7-alpine",
+                healthcheck: {
+                    test: ["CMD-SHELL", "redis-cli ping | grep PONG"]
+                },
+                networks: {
+                    internet: {
+                        aliases: ["redis"]
+                    },
+                    lan_dialer: {
+                        aliases: ["redis"]
+                    },
+                    lan_listener: {
+                        aliases: ["redis"]
+                    },
+                }
+            }
+        },
+        networks: {
+            lan_dialer: {},
+            lan_listener: {},
+            internet: {},
+        }
+    }
+}
---
+++ hole-punch-interop/testplans.ts
@@ -0,0 +1,131 @@
+import { buildTestSpecs } from "./src/generator"
+import { Version, versions } from "./versions"
+import { promises as fs } from "fs";
+import {ExecException, run} from "./src/compose-runner"
+import { stringify } from "csv-stringify/sync"
+import { stringify as YAMLStringify } from "yaml"
+import yargs from "yargs/yargs"
+import path from "path";
+
+(async () => {
+    const WorkerCount = parseInt(process.env.WORKER_COUNT || "1")
+    const argv = await yargs(process.argv.slice(2))
+        .options({
+            'name-filter': {
+                description: 'Only run tests including this name',
+                default: "",
+            },
+            'name-ignore': {
+                description: 'Do not run any tests including this name',
+                default: "",
+            },
+            'dry-run': {
+                description: "Don't actually run the test, just generate the compose files",
+                default: false,
+                type: 'boolean'
+            },
+            'extra-versions-dir': {
+                description: 'Look for extra versions in this directory. Version files must be in json format',
+                default: "",
+                type: 'string'
+            },
+            'extra-version': {
+                description: 'Paths to JSON files for additional versions to include in the test matrix',
+                default: [],
+                type: 'array'
+            },
+        })
+        .help()
+        .version(false)
+        .alias('help', 'h').argv;
+    const extraVersionsDir = argv.extraVersionsDir
+    const extraVersions: Array<Version> = []
+    if (extraVersionsDir !== "") {
+        try {
+            const files = await fs.readdir(extraVersionsDir);
+            for (const file of files) {
+                const contents = await fs.readFile(path.join(extraVersionsDir, file))
+                extraVersions.push(...JSON.parse(contents.toString()))
+            }
+        } catch (err) {
+            console.error("Error reading extra versions")
+            console.error(err);
+        }
+    }
+
+    for (let versionPath of argv.extraVersion.filter(p => p !== "")) {
+        const contents = await fs.readFile(versionPath);
+        extraVersions.push(JSON.parse(contents.toString()))
+    }
+
+    let nameFilter: string | null = argv["name-filter"]
+    if (nameFilter === "") {
+        nameFilter = null
+    }
+    let nameIgnore: string | null = argv["name-ignore"]
+    if (nameIgnore === "") {
+        nameIgnore = null
+    }
+
+    let routerImageId = JSON.parse(await fs.readFile(path.join(".", "router", "image.json"), "utf-8")).imageID;
+    let relayImageId = JSON.parse(await fs.readFile(path.join(".", "rust-relay", "image.json"), "utf-8")).imageID;
+
+    const routerDelay = 100;
+    const relayDelay = 25;
+
+    const rttRelayedConnection = routerDelay * 2 + relayDelay * 2;
+    const rttDirectConnection = routerDelay * 2;
+
+    const assetDir = path.join(__dirname, "runs");
+
+    let testSpecs = await buildTestSpecs(versions.concat(extraVersions), nameFilter, nameIgnore, routerImageId, relayImageId, routerDelay, relayDelay, assetDir)
+
+    console.log(`Running ${testSpecs.length} tests`)
+    const failures: Array<{ name: String, e: ExecException }> = []
+    const statuses: Array<string[]> = [["name", "outcome"]]
+    const workers = new Array(WorkerCount).fill({}).map(async () => {
+        while (true) {
+            const testSpec = testSpecs.pop()
+            if (testSpec == null) {
+                return
+            }
+            const name = testSpec.name;
+            if (!name) {
+                console.warn("Skipping testSpec without name")
+                continue;
+            }
+
+            console.log("Running test spec: " + name)
+
+            try {
+                const report = await run(testSpec, assetDir, argv['dry-run'] as boolean);
+
+                if (report != null) {
+                    const rttDifference = Math.abs(report.rtt_to_holepunched_peer_millis - rttDirectConnection);
+
+                    if (rttDifference > 5) {
+                        // Emit a warning but don't do anything for now.
+                        console.warn(`Expected RTT of direct connection to be ~${rttDirectConnection}ms but was ${report.rtt_to_holepunched_peer_millis}ms`)
+                    }
+                }
+
+                statuses.push([name, "success"])
+            } catch (e) {
+                failures.push({ name, e })
+                statuses.push([name, "failure"])
+            }
+        }
+    })
+    await Promise.all(workers)
+
+    console.log(`${failures.length} failures:`)
+
+    for (const [number, {name, e}] of failures.entries()) {
+        console.log(`---------- ${name} ---------- (${number + 1} / ${failures.length})\n`);
+        console.log(e.stderr)
+    }
+
+    await fs.writeFile("results.csv", stringify(statuses))
+
+    console.log("Run complete")
+})()
