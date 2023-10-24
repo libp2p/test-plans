d432e9a7b21418bec8934ba194ba3339c7b278ed
Thomas Eizinger <thomas@eizinger.io>
2023-10-24 16:22:45 +1100

    Statically link on default target

--- hole-punch-interop/dockerBuildWrapper.sh
+++ hole-punch-interop/dockerBuildWrapper.sh
@@ -8,24 +8,6 @@ if [[ -n "${CI}" ]] && [[ -n "${AWS_BUCKET}" ]]; then
         --cache-from type=s3,mode=max,bucket=$AWS_BUCKET,region=$AWS_REGION,prefix=buildCache,name=$IMAGE_NAME"
 fi
 
-# Detect the architecture
-ARCH=$(uname -m)
-
-# Set the Rust target to use musl
-case "$ARCH" in
-    x86_64)
-        TARGET="x86_64-unknown-linux-musl"
-        ;;
-    arm64)
-        TARGET="aarch64-unknown-linux-musl"
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH" >&2
-        exit 1
-        ;;
-esac
-
 docker buildx build \
     --load \
-    --build-arg RUST_TARGET=$TARGET \
     -t $IMAGE_NAME $CACHING_OPTIONS "$@"
