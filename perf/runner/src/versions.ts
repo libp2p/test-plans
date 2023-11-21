9a4c96952368e6432fe51fd12008d31ca30f63ea
Alex Potsides <alex@achingbrain.net>
2023-11-21 08:22:56 +0000

    fix: re-enable js-libp2p under node.js (#325)
    Adds perf tests for js-libp2p 0.46.x and 1.0 (via the `next` tag).
    
    ---------
    
    Co-authored-by: achingbrain <achingbrain@users.noreply.github.com>

--- perf/runner/src/versions.ts
+++ perf/runner/src/versions.ts
@@ -40,4 +40,14 @@ export const versions: Array<Version> = [
         implementation: "go-libp2p",
         transportStacks: ["tcp", "quic-v1"]
     },
+    {
+        id: "v0.46",
+        implementation: "js-libp2p",
+        transportStacks: ["tcp"]
+    },
+    {
+        id: "v1.0",
+        implementation: "js-libp2p",
+        transportStacks: ["tcp"]
+    },
 ]
