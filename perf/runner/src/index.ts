ea64c29e8dc89ed7535f11d52ac136e0ccf73af7
achingbrain <alex@achingbrain.net>
2023-11-21 15:00:27 +0000

    chore: restore server log

--- perf/runner/src/index.ts
+++ perf/runner/src/index.ts
@@ -134,7 +134,7 @@ function runBenchmarkAcrossVersions(args: ArgsRunBenchmarkAcrossVersions): Bench
         const killSTDOUT = execCommand(killCMD);
         console.error(killSTDOUT);
 
-        const serverCMD = `ssh -o StrictHostKeyChecking=no ec2-user@${args.serverPublicIP} 'nohup ./impl/${version.implementation}/${version.id}/perf --run-server --server-address 0.0.0.0:4001 & echo \$! > pidfile '`;
+        const serverCMD = `ssh -o StrictHostKeyChecking=no ec2-user@${args.serverPublicIP} 'nohup ./impl/${version.implementation}/${version.id}/perf --run-server --server-address 0.0.0.0:4001 > server.log 2>&1 & echo \$! > pidfile '`;
         const serverSTDOUT = execCommand(serverCMD);
         console.error(serverSTDOUT);
 
