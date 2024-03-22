8f37511b6b46a7a735d4a5a79c484ed293bbe02a
galargh <piotr.galar@gmail.com>
2024-03-22 15:33:04 +0000

    feat: add new implementation versions (go)

--- perf/runner/src/versions.ts
+++ perf/runner/src/versions.ts
@@ -1,53 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+
 export type Version = {
     id: string,
     implementation: "go-libp2p" | "js-libp2p" | "nim-libp2p" | "rust-libp2p" | "zig-libp2p" | "https" | "quic-go",
     transportStacks: string[],
 }
 
-export const versions: Array<Version> = [
-    {
-        id: "v0.34",
-        implementation: "quic-go",
-        transportStacks: ["quic-v1"]
-    },
-    {
-        id: "v0.53",
-        implementation: "rust-libp2p",
-        transportStacks: ["tcp", "quic-v1"]
-    },
-    {
-        id: "v0.1",
-        implementation: "https",
-        transportStacks: ["tcp"]
-    },
-    {
-        id: "v0.27",
-        implementation: "go-libp2p",
-        transportStacks: ["tcp", "quic-v1"]
-    },
-    {
-        id: "v0.28",
-        implementation: "go-libp2p",
-        transportStacks: ["tcp", "quic-v1"]
-    },
-    {
-        id: "v0.29",
-        implementation: "go-libp2p",
-        transportStacks: ["tcp", "quic-v1"]
-    },
-    {
-        id: "v0.31",
-        implementation: "go-libp2p",
-        transportStacks: ["tcp", "quic-v1"]
-    },
-    {
-        id: "v0.46",
-        implementation: "js-libp2p",
-        transportStacks: ["tcp"]
-    },
-    {
-        id: "v1.0",
-        implementation: "js-libp2p",
-        transportStacks: ["tcp"]
-    },
-]
+export const versions: Array<Version> = JSON.parse(fs.readFileSync(path.join(__dirname, '../versions.json'), 'utf8'));
