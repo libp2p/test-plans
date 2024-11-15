5f7b668f97748844057fb78e66f051b1ee5626ca
Marco Munizaga <git@marcopolo.io>
2024-11-14 16:20:34 -0800

    update to rust v0.54

--- hole-punch-interop/versions.ts
+++ hole-punch-interop/versions.ts
@@ -1,26 +1,26 @@
 import fs from "fs"
 
 export type Version = {
-    id: string,
-    // This can be the image ID, or a function that takes the version ID and returns the image ID.
-    // By default it uses the canonicalImageIDLookup.
-    containerImageID?: string,
-    transports: Array<"tcp" | "quic">,
+  id: string,
+  // This can be the image ID, or a function that takes the version ID and returns the image ID.
+  // By default it uses the canonicalImageIDLookup.
+  containerImageID?: string,
+  transports: Array<"tcp" | "quic">,
 }
 
 export const versions: Array<Version> = [
-    {
-        id: "go-v0.35",
-        transports: ["tcp", "quic"]
-    },
-    {
-        id: "rust-v0.53",
-        transports: ["tcp", "quic"]
-    },
+  {
+    id: "go-v0.35",
+    transports: ["tcp", "quic"]
+  },
+  {
+    id: "rust-v0.54",
+    transports: ["tcp", "quic"]
+  },
 ].map((v: Version) => (typeof v.containerImageID === "undefined" ? ({ ...v, containerImageID: readImageId(canonicalImagePath(v.id)) }) : v))
 
 function readImageId(path: string): string {
-    return JSON.parse(fs.readFileSync(path, "utf8")).imageID;
+  return JSON.parse(fs.readFileSync(path, "utf8")).imageID;
 }
 
 // Finds the `image.json` for the given version id.
@@ -28,15 +28,15 @@ function readImageId(path: string): string {
 // Expects the form of "<impl>-vX.Y.Z" or "<impl>vX.Y".
 // The image id must be in the file "./impl/<impl>/vX.Y/image.json" or "./impl/<impl>/v0.0.Z/image.json".
 function canonicalImagePath(id: string): string {
-    // Split by implementation and version
-    const [impl, version] = id.split("-v")
-    // Drop the patch version
-    const [major, minor, patch] = version.split(".")
-    let versionFolder = `v${major}.${minor}`
-    if (major === "0" && minor === "0") {
-        // We're still in the 0.0.x phase, so we use the patch version
-        versionFolder = `v0.0.${patch}`
-    }
-    // Read the image ID from the JSON file on the filesystem
-    return `./impl/${impl}/${versionFolder}/image.json`
+  // Split by implementation and version
+  const [impl, version] = id.split("-v")
+  // Drop the patch version
+  const [major, minor, patch] = version.split(".")
+  let versionFolder = `v${major}.${minor}`
+  if (major === "0" && minor === "0") {
+    // We're still in the 0.0.x phase, so we use the patch version
+    versionFolder = `v0.0.${patch}`
+  }
+  // Read the image ID from the JSON file on the filesystem
+  return `./impl/${impl}/${versionFolder}/image.json`
 }
