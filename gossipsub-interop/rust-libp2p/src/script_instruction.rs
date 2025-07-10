324086a9cf94e9c20a031aca3be0e103caa19527
Marco Munizaga <git@marcopolo.io>
2025-07-10 14:58:23 -0700

    Fix Gossipsub param parsing in Rust

--- gossipsub-interop/rust-libp2p/src/script_instruction.rs
+++ gossipsub-interop/rust-libp2p/src/script_instruction.rs
@@ -51,53 +51,43 @@ impl From<NodeID> for Keypair {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum ScriptInstruction {
-    #[serde(rename = "connect")]
-    Connect {
-        #[serde(rename = "connectTo")]
-        connect_to: Vec<NodeID>,
-    },
+    #[serde(rename = "connect", rename_all = "camelCase")]
+    Connect { connect_to: Vec<NodeID> },
 
-    #[serde(rename = "ifNodeIDEquals")]
+    #[serde(rename = "ifNodeIDEquals", rename_all = "camelCase")]
     IfNodeIDEquals {
         #[serde(rename = "nodeID")]
         node_id: NodeID,
-        #[serde(rename = "instruction")]
         instruction: Box<ScriptInstruction>,
     },
 
-    #[serde(rename = "waitUntil")]
-    WaitUntil {
-        #[serde(rename = "elapsedSeconds")]
-        elapsed_seconds: u64,
-    },
+    #[serde(rename = "waitUntil", rename_all = "camelCase")]
+    WaitUntil { elapsed_seconds: u64 },
 
-    #[serde(rename = "publish")]
+    #[serde(rename = "publish", rename_all = "camelCase")]
     Publish {
         #[serde(rename = "messageID")]
         message_id: u64,
-        #[serde(rename = "messageSizeBytes")]
         message_size_bytes: usize,
         #[serde(rename = "topicID")]
         topic_id: String,
     },
 
-    #[serde(rename = "subscribeToTopic")]
+    #[serde(rename = "subscribeToTopic", rename_all = "camelCase")]
     SubscribeToTopic {
         #[serde(rename = "topicID")]
         topic_id: String,
     },
 
-    #[serde(rename = "setTopicValidationDelay")]
+    #[serde(rename = "setTopicValidationDelay", rename_all = "camelCase")]
     SetTopicValidationDelay {
         #[serde(rename = "topicID")]
         topic_id: String,
-        #[serde(rename = "delaySeconds")]
         delay_seconds: f64,
     },
 
-    #[serde(rename = "initGossipSub")]
+    #[serde(rename = "initGossipSub", rename_all = "camelCase")]
     InitGossipSub {
-        #[serde(rename = "gossipSubParams")]
         gossip_sub_params: Box<GossipSubParams>,
     },
 }
@@ -124,61 +114,54 @@ impl ExperimentParams {
 
 /// GossipSubParams contains parameters for the GossipSub protocol.
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, Default)]
+#[serde(rename_all = "PascalCase")]
 pub struct GossipSubParams {
     #[serde(rename = "D")]
     pub d: Option<usize>,
-    #[serde(rename = "D_low")]
+    #[serde(rename = "Dlo")]
     pub d_low: Option<usize>,
-    #[serde(rename = "D_high")]
+    #[serde(rename = "Dhi")]
     pub d_high: Option<usize>,
-    #[serde(rename = "D_score")]
+    #[serde(rename = "Dscore")]
     pub d_score: Option<usize>,
-    #[serde(rename = "D_out")]
+    #[serde(rename = "Dout")]
     pub d_out: Option<usize>,
-    #[serde(rename = "historyLength")]
     pub history_length: Option<usize>,
-    #[serde(rename = "historyGossip")]
     pub history_gossip: Option<usize>,
-    #[serde(rename = "heartbeatInitialDelay")]
+    #[serde(rename = "Dlazy")]
+    pub d_lazy: Option<usize>,
+    pub gossip_factor: Option<f64>,
+    pub gossip_retransmission: Option<usize>,
     pub heartbeat_initial_delay: Option<f64>,
-    #[serde(rename = "heartbeatInterval")]
     pub heartbeat_interval: Option<f64>,
-    #[serde(rename = "fanoutTTL")]
+    pub slow_heartbeat_warning: Option<f64>,
+    #[serde(rename = "FanoutTTL")]
     pub fanout_ttl: Option<f64>,
-    #[serde(rename = "mcacheLen")]
-    pub mcache_len: Option<i32>,
-    #[serde(rename = "mcacheGossip")]
-    pub mcache_gossip: Option<i32>,
-    #[serde(rename = "seenTTL")]
-    pub seen_ttl: Option<f64>,
-    #[serde(rename = "opportunisticGraftPeers")]
-    pub opportunistic_graft_peers: Option<i32>,
-    #[serde(rename = "opportunisticGraftTicks")]
-    pub opportunistic_graft_ticks: Option<i32>,
-    #[serde(rename = "opportunisticGraftTicksBackoff")]
-    pub opportunistic_graft_ticks_backoff: Option<i32>,
-    #[serde(rename = "directConnectTicks")]
-    pub direct_connect_ticks: Option<i32>,
-    #[serde(rename = "directConnectTicksBackoff")]
-    pub direct_connect_ticks_backoff: Option<i32>,
-    #[serde(rename = "directConnectInitialDelay")]
-    pub direct_connect_initial_delay: Option<f64>,
-    #[serde(rename = "pruneBackoff")]
+    pub prune_peers: Option<usize>,
     pub prune_backoff: Option<f64>,
-    #[serde(rename = "floodPublish")]
-    pub flood_publish: Option<bool>,
-    #[serde(rename = "graftFloodThreshold")]
-    pub graft_flood_threshold: Option<i32>,
-    #[serde(rename = "validateMessageDeliveriesWindow")]
-    pub validate_message_deliveries_window: Option<f64>,
-    #[serde(rename = "scoreInspectPeersCacheSize")]
-    pub score_inspect_peers_cache_size: Option<i32>,
-    #[serde(rename = "maxIHaveLength")]
+    pub unsubscribe_backoff: Option<f64>,
+    pub connectors: Option<usize>,
+    pub max_pending_connections: Option<usize>,
+    pub connection_timeout: Option<f64>,
+    pub direct_connect_ticks: Option<i32>,
+    pub direct_connect_initial_delay: Option<f64>,
+    pub opportunistic_graft_ticks: Option<u64>,
+    pub opportunistic_graft_peers: Option<usize>,
+    pub graft_flood_threshold: Option<f64>,
+    #[serde(rename = "MaxIHaveLength")]
     pub max_ihave_length: Option<usize>,
-    #[serde(rename = "maxIHaveMessages")]
+    #[serde(rename = "MaxIHaveMessages")]
     pub max_ihave_messages: Option<usize>,
-    #[serde(rename = "iWantFollowupTime")]
+    #[serde(rename = "MaxIDontWantLength")]
+    pub max_idont_want_length: Option<usize>,
+    #[serde(rename = "MaxIDontWantMessages")]
+    pub max_idont_want_messages: Option<usize>,
+    #[serde(rename = "IWantFollowupTime")]
     pub iwant_followup_time: Option<f64>,
+    #[serde(rename = "IDontWantMessageThreshold")]
+    pub idont_want_message_threshold: Option<usize>,
+    #[serde(rename = "IDontWantMessageTTL")]
+    pub idont_want_message_ttl: Option<f64>,
 }
 
 impl From<GossipSubParams> for ConfigBuilder {
@@ -193,6 +176,27 @@ impl From<GossipSubParams> for ConfigBuilder {
         if let Some(d_high) = params.d_high {
             builder.mesh_n_high(d_high);
         }
+        if let Some(d_score) = params.d_score {
+            builder.retain_scores(d_score);
+        }
+        if let Some(d_out) = params.d_out {
+            builder.mesh_outbound_min(d_out);
+        }
+        if let Some(history_length) = params.history_length {
+            builder.history_length(history_length);
+        }
+        if let Some(history_gossip) = params.history_gossip {
+            builder.history_gossip(history_gossip);
+        }
+        if let Some(d_lazy) = params.d_lazy {
+            builder.gossip_lazy(d_lazy);
+        }
+        if let Some(gossip_factor) = params.gossip_factor {
+            builder.gossip_factor(gossip_factor);
+        }
+        if let Some(gossip_retransmission) = params.gossip_retransmission {
+            builder.gossip_retransimission(gossip_retransmission as u32);
+        }
         if let Some(heartbeat_initial_delay) = params.heartbeat_initial_delay {
             builder.heartbeat_initial_delay(Duration::from_nanos(heartbeat_initial_delay as u64));
         }
@@ -202,14 +206,23 @@ impl From<GossipSubParams> for ConfigBuilder {
         if let Some(fanout_ttl) = params.fanout_ttl {
             builder.fanout_ttl(Duration::from_nanos(fanout_ttl as u64));
         }
-        if let Some(history_length) = params.history_length {
-            builder.history_length(history_length);
+        if let Some(prune_peers) = params.prune_peers {
+            builder.prune_peers(prune_peers);
         }
-        if let Some(history_gossip) = params.history_gossip {
-            builder.history_gossip(history_gossip);
+        if let Some(prune_backoff) = params.prune_backoff {
+            builder.prune_backoff(Duration::from_nanos(prune_backoff as u64));
         }
-        if let Some(flood_publish) = params.flood_publish {
-            builder.flood_publish(flood_publish);
+        if let Some(unsubscribe_backoff) = params.unsubscribe_backoff {
+            builder.unsubscribe_backoff(Duration::from_nanos(unsubscribe_backoff as u64).as_secs());
+        }
+        if let Some(opportunistic_graft_ticks) = params.opportunistic_graft_ticks {
+            builder.opportunistic_graft_ticks(opportunistic_graft_ticks);
+        }
+        if let Some(opportunistic_graft_peers) = params.opportunistic_graft_peers {
+            builder.opportunistic_graft_peers(opportunistic_graft_peers);
+        }
+        if let Some(graft_flood_threshold) = params.graft_flood_threshold {
+            builder.graft_flood_threshold(Duration::from_nanos(graft_flood_threshold as u64));
         }
         if let Some(max_ihave_length) = params.max_ihave_length {
             builder.max_ihave_length(max_ihave_length);
@@ -219,6 +232,9 @@ impl From<GossipSubParams> for ConfigBuilder {
         }
         if let Some(iwant_followup_time) = params.iwant_followup_time {
             builder.iwant_followup_time(Duration::from_nanos(iwant_followup_time as u64));
+        }
+        if let Some(idont_want_message_threshold) = params.idont_want_message_threshold {
+            builder.idontwant_message_size_threshold(idont_want_message_threshold);
         }
 
         // Just disable this by using a large value
