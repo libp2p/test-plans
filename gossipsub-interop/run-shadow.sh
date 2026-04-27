ee232db2e034d90704f360a8f9ee6ed0b1b1cfcd
bomanaps <mercy.boma35@yahoo.com>
2026-04-27 17:08:09 +0100

    fix: use cd instead of --build-file for zig build

--- gossipsub-interop/run-shadow.sh
+++ gossipsub-interop/run-shadow.sh
@@ -16,8 +16,7 @@ echo "==> Building gossipsub-bin for linux/x86_64..."
 # -Dcpu overrides the auto-detected CPU (e.g. 'athlon-xp' under Rosetta/QEMU
 # which newer LLVM doesn't recognise) while keeping native mode so Zig still
 # uses system headers (needed for zlib.h etc).
-zig build \
-    --build-file /eth-p2p-z/build.zig \
+cd /eth-p2p-z && zig build \
     --prefix /work/zig-libp2p/zig-out \
     -Doptimize=ReleaseFast \
     -Dcpu=x86_64
